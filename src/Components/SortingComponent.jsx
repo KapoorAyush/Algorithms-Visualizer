00d43a42935abc04b364e85d2469e8b0214b15a2
Ayush Kapoor <42859178+KapoorAyush@users.noreply.github.com>
2021-07-22 18:40:16 +0530

    Selection Sort

--- src/Components/SortingComponent.jsx
+++ src/Components/SortingComponent.jsx
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 import './SortingC.css';
+function delay(delayInms) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(2);
+      }, delayInms);
+    });
+  }
 class SortingComponent extends Component{
     constructor(props){
         super(props);
@@ -18,11 +25,32 @@ class SortingComponent extends Component{
         }
         this.setState({array});
     }
-    
+    async selectionSort(){
+        const arrayBars = document.getElementsByClassName('array-bar');
+        var arr=this.state.array;
+        for (let i=0; i<100;i++){
+            var min=i;
+            for(let j=i+1;j<100;j++){
+                if(arr[min]>arr[j]){
+                    arrayBars[j].style.backgroundColor="cyan";
+                    let dele =await delay(0.5)
+                    let temp=arr[min];
+                    arr[min]=arr[j];
+                    arr[j]=temp;
+                    arrayBars[j].style.backgroundColor="black";
+                    arrayBars[min].style.backgroundColor="cyan";
+                }
+                this.setState({arr});
+            }
+            arrayBars[i].style.backgroundColor="green"
+        }
+        this.setState({arr});
+    }
     render(){
         return(
             <div className="array-container">
             <button onClick={() => this.generateArray()}>Generate New Array</button>
+            <button onClick={() => this.selectionSort()}>Selection Sort</button>
             <br/>
             {this.state.array.map((val, i) => (
                 <div className="array-bar" key={i}
