e7106b97844f563ae73d9174e7e9cee015ecdcef
Ayush Kapoor <42859178+KapoorAyush@users.noreply.github.com>
2021-07-23 18:02:40 +0530

    temp. changes

--- src/Components/SortingComponent.jsx
+++ src/Components/SortingComponent.jsx
@@ -11,43 +11,75 @@ class SortingComponent extends Component{
     constructor(props){
         super(props);
         this.state={
-            array:[]
+            array:[],
+            N:100
         }
+        this.handleInputChange = this.handleInputChange.bind(this);
+
+    }
+    handleInputChange(event) {
+
+        const target = event.target;
+        const value = target.value;
+        this.setState({
+          N: value
+        });
+        console.log(this.state.N)
+        this.generateArray()
     }
     componentDidMount() {
         this.generateArray();
+        // this.setState({N:document.getElementById("N").value});
     }
     
     generateArray() {
         const array = [];
-        for (let i = 0; i <100; i++) {
+        for (let i = 0; i <this.state.N; i++) {
           array.push(Math.round(Math.random() * 1000));
         }
-        this.setState({array});
+        this.setState({array:array});
+
     }
     async selectionSort(){
         const arrayBars = document.getElementsByClassName('array-bar');
         var arr=this.state.array;
-        for (let i=0; i<100;i++){
+        for (let i=0; i<this.state.N;i++){
             var min=i;
-            for(let j=i+1;j<100;j++){
+            for(let j=i+1;j<this.state.N;j++){
+                arrayBars[i].style.backgroundColor="red";
+                arrayBars[j].style.backgroundColor="cyan";
+                let dele =await delay(1)
                 if(arr[min]>arr[j]){
-                    arrayBars[j].style.backgroundColor="cyan";
-                    let dele =await delay(0.5)
-                    let temp=arr[min];
-                    arr[min]=arr[j];
-                    arr[j]=temp;
-                    arrayBars[j].style.backgroundColor="black";
-                    arrayBars[min].style.backgroundColor="cyan";
+                    arrayBars[min].style.backgroundColor="black";
+                    arrayBars[j].style.backgroundColor="gray";
+                    // let dele =await delay(1)
+                    min=j;
                 }
-                this.setState({arr});
+                else
+                    arrayBars[j].style.backgroundColor="black";
+                this.setState({array:arr});
+            }
+            if(i!=min){
+                let temp=arr[i];
+                arr[i]=arr[min];
+                arr[min]=temp;
+                arrayBars[min].style.backgroundColor="black";
             }
             arrayBars[i].style.backgroundColor="green"
         }
-        this.setState({arr});
+        for (let item of arrayBars) {
+            let dele =await delay(0.5)
+            item.style.backgroundColor="black"
+        }
+        // this.setState({arr});
     }
+
     render(){
         return(
+            <div> 
+            <div>
+                <input type="range" id="N" name="N" min="20" max="200" step="1" value={this.state.N} onChange={this.handleInputChange}/>
+            </div>
             <div className="array-container">
             <button onClick={() => this.generateArray()}>Generate New Array</button>
             <button onClick={() => this.selectionSort()}>Selection Sort</button>
@@ -55,11 +87,14 @@ class SortingComponent extends Component{
             {this.state.array.map((val, i) => (
                 <div className="array-bar" key={i}
                 style={{
-                height: `${val*0.08}vh`,
+                    width: `${500/this.state.N}px`,
+                    height: `${val*0.08}vh`,
                 }}>
                 </div>
             ))}
             </div>
+            </div>
+
         );
     }
 }
